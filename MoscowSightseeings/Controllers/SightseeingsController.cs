fda7e11abd220237bf33b76367a97c833970c665
Saqo <saqoagadzhanyan@gmail.com>
2015-01-29 01:49:25 +0400

    v 1.0

---
+++ MoscowSightseeings/Controllers/SightseeingsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MoscowSightseeings.Models;
+
+namespace MoscowSightseeings.Controllers
+{
+    public class SightseeingsController : Controller
+    {
+        //
+        // GET: /Sightseeings/
+
+        public ActionResult Index()
+        {
+
+
+            DataManager db = new DataManager();
+
+            ViewData.Model = db.GetItems();
+            return View();
+        }
+
+        public ActionResult Place(Guid ID, string content)
+        {
+            DataManager db = new DataManager();
+            Places place = db.GetItemByID(ID);
+            return View(place);
+        }
+    }
+}
---
+++ MoscowSightseeings/Models/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoscowSightseeings.Models
+{
+    public class DataManager
+    {
+        private PlacesListDBEntities5 _db;
+
+        public DataManager()
+        {
+            _db = new PlacesListDBEntities5();
+        }
+
+        public IQueryable<Places> GetItems()
+        {
+            return _db.Places;
+        }
+
+        public Places GetItemByID(Guid ID)
+        {
+            return _db.Places.FirstOrDefault(x => x.PlaceId == ID);
+        }
+
+    }
+}
